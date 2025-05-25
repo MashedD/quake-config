d30a8e313b9b130e2b006aecea44e65f1b83d338
MashedD <sl8code@gmail.com>
2025-05-25 12:02:45 +0200

    remove aqtion

--- q2pro/clean.sh
+++ q2pro/clean.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 rm -f items.txt
-rm -f {3zb2,action,baseq2,dday,jump,openffa,opentdm}/{config.cfg,.conhistory,.syshistory,q2config.cfg}
+rm -f {3zb2,baseq2,dday,jump,openffa,opentdm}/{config.cfg,.conhistory,.syshistory,q2config.cfg}
 
