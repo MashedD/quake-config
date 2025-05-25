de2c2eece4827c04db071caa29d52b9b32e0e59a
MashedD <sl8code@gmail.com>
2025-05-25 09:28:04 +0200

    change crosshair

--- q2pro/q2pro.sh
+++ q2pro/q2pro.sh
@@ -10,10 +10,11 @@ cd "$GAME_PATH"
 TOUCHPAD_NAME=${TOUCHPAD_NAME:-"ELAN0300:00 04F3:3206 Touchpad"}
 xinput disable "$TOUCHPAD_NAME"
 
-#WIDTH=800 HEIGHT=600 \
 EXEC=""
 [ -n "$(command -v gamemoderun 2>/dev/null)" ] && EXEC="$EXEC gamemoderun"
-[ -n "$(command -v gamescope 2>/dev/null)" ] && EXEC="$EXEC gamescope -W $WIDTH -H $HEIGHT -r $REFRESH --force-grab-cursor -f --"
+[ -n "$(command -v gamescope 2>/dev/null)" ] && \
+    WIDTH=800 HEIGHT=600 \
+    EXEC="$EXEC gamescope -W $WIDTH -H $HEIGHT -r $REFRESH --force-grab-cursor -f --"
 $EXEC ./q2pro +set homedir . "$@"
 
 # Restore touchpad? nah, don't care...
