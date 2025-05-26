be9d3f4a03974e4b8197d22c5dce7f8f8703ffc3
MashedD <sl8code@gmail.com>
2025-05-26 04:58:52 +0200

    configs change

--- q2pro/q2pro.sh
+++ q2pro/q2pro.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
-GAME_PATH=${GAME_PATH:-"$HOME/Games/q2pro-test"}
+GAME_PATH=${GAME_PATH:-"$HOME/Games/q2pro"}
 WIDTH=${WIDTH:-1920}
 HEIGHT=${HEIGHT:-1080}
 REFRESH=${REFRESH:-144}
