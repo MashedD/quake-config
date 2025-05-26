5de123a6f0fd74196cdc2f6a89c9044e41902ec0
MashedD <sl8code@gmail.com>
2025-05-26 07:02:11 +0200

    add fix for switching from 3zb2 to jump mod

--- q2pro/clean.sh
+++ q2pro/clean.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 rm -f items.txt
-rm -f {3zb2,baseq2,dday,jump,openffa,opentdm}/{config.cfg,.conhistory,.syshistory,q2config.cfg}
+rm -f {3zb2,baseq2,dday,jump,openffa,opentdm}/{config.cfg,.conhistory,.syshistory,q2config.cfg,debug.log}
+#rm -rf {3zb2,baseq2,dday,jump,openffa,opentdm}/demos
+rm -rf jump/{27910,ent,global,jumpdemo,mapsent}
 
